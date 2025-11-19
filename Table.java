aa19213f2af79c9570f579c9c4898d1213c17c5f
Teddy <hei.teddy.2@gmail.com>
2025-11-19 09:13:08 +0300

    Initial commit

---
+++ Table.java
@@ -0,0 +1,39 @@
+public class Table {
+    private float longueur;
+    private float largeur;
+    private String etat;
+    private String couleur;
+
+    public float getLongueur() {
+        return longueur;
+    }
+
+    public void setLongueur(float longueur) {
+        this.longueur = longueur;
+    }
+
+    public float getLargeur() {
+        return largeur;
+    }
+
+    public void setLargeur(float largeur) {
+        this.largeur = largeur;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+}
